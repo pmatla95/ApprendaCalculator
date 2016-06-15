f569d83cf09ce472057e286ccfbd17c1736e402d
pmatla95 <pmatla95@gmail.com>
2016-06-15 11:27:53 -0400

    Added DB and DB access to store calculations

---
+++ ApplicationProvisioning_Script.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [CalculationAudit](
+[operand1] [float],
+[operand2] [float],
+[operator] [nchar](1),
+[result] [float]
+)
+GO
--- Calculator/Service/Calculator.cs
+++ Calculator/Service/Calculator.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.ServiceModel.Web;
 using System.Text;
+using System.Configuration;
+using System.Data.Common;
 
 namespace CalculatorService
 {
@@ -11,32 +13,119 @@ namespace CalculatorService
         public Calculation Add(Calculation data)
         {
             data.Result = data.Operand1 + data.Operand2;
+            AuditCalculation(data, "+");
             return data;
         }
         public Calculation Subtract(Calculation data)
         {
             data.Result = data.Operand1 - data.Operand2;
+            AuditCalculation(data, "-");
             return data;
         }
         public Calculation Multiply(Calculation data)
         {
             data.Result = data.Operand1 * data.Operand2;
+            AuditCalculation(data, "*");
             return data;
         }
         public Calculation Divide(Calculation data)
         {
             data.Result = data.Operand1 / data.Operand2;
+            AuditCalculation(data, "/");
             return data;
         }
         public Calculation SquareRoot(Calculation data)
         {
             data.Result = Math.Sqrt(data.Operand1);
+            AuditCalculation(data, "R");
             return data;
         }
+
+
+
         public string[] GetCalculationAudits()
         {
-            throw new NotImplementedException();
+            List<string> results = new List<string>();
+            DbProviderFactory providerFactory =
+           DbProviderFactories.GetFactory(ConfigurationManager
+           .ConnectionStrings["CalculatorDB"].ProviderName);
+            using (DbConnection conn = providerFactory.CreateConnection())
+            {
+                conn.ConnectionString = ConfigurationManager
+                .ConnectionStrings["CalculatorDB"]
+                .ConnectionString;
+                DbCommand command = conn.CreateCommand();
+                command.CommandText =
+               "select operand1, operand2, " +
+                "operator, result from calculationaudit";
+                conn.Open();
+                using (DbDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string mathOperator = (string)reader["operator"];
+                        if ("R".Equals(mathOperator))
+                        {
+                            results.Add(string.Format("sqrt({0}) = {1}",
+                            reader["operand1"],
+                           reader["result"]));
+                        }
+                        else
+                        {
+                            results.Add(string.Format("{0} {1} {2} = {3}",
+                            reader["operand1"],
+                           reader["operator"],
+                           reader["operand2"],
+                            reader["result"]));
+                        }
+                    }
+                }
+            }
+            return results.ToArray();
         }
+
+
+        //INSERTS DATA into single table DB
+        private static void AuditCalculation(Calculation data, string mathOperatorString)
+        {
+            DbProviderFactory providerFactory =
+             DbProviderFactories.GetFactory(
+            ConfigurationManager.ConnectionStrings["CalculatorDB"]
+             .ProviderName);
+            using (DbConnection conn = providerFactory.CreateConnection())
+            {
+                conn.ConnectionString =
+                ConfigurationManager
+                .ConnectionStrings["CalculatorDB"]
+                 .ConnectionString;
+                DbCommand command = conn.CreateCommand();
+                command.CommandText =
+                "INSERT INTO CALCULATIONAUDIT" +
+               "(operand1,operand2,operator,result) " +
+               "VALUES(@operand1,@operand2,@operator,@result)";
+                DbParameter operand1 = command.CreateParameter();
+                operand1.ParameterName = "@operand1";
+                operand1.Value = data.Operand1;
+                DbParameter operand2 = command.CreateParameter();
+                operand2.ParameterName = "@operand2";
+                operand2.Value = data.Operand2;
+                DbParameter mathOperator = command.CreateParameter();
+                mathOperator.ParameterName = "@operator";
+                mathOperator.Value = mathOperatorString;
+                DbParameter result = command.CreateParameter();
+                result.ParameterName = "@result";
+                result.Value = data.Result;
+                command.Parameters.Add(operand1);
+                command.Parameters.Add(operand2);
+                command.Parameters.Add(mathOperator);
+                command.Parameters.Add(result);
+                conn.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+
+
         public Stream GetClientAccessPolicy()
         {
             const string result =
